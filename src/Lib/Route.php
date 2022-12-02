eba9d3ecb12db033712a71f36f4b9a30341f9a72
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-02 16:59:59 +0800

    added Route

---
+++ src/Lib/Route.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lib;
+
+class Route
+{   
+    private $url;
+    private $method;
+    private $callback;
+    private $params = [];
+
+    public function __construct(string $url, string $method, callable $callback)
+    {
+        $this->url = $url;
+        $this->method = $method;
+        $this->callback = $callback;
+    }
+
+    public function params(): array
+    {
+        return $this->params;
+    }
+}
