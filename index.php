148eb622f0c0eac9c7fe86d437cdb7a5bb86ce1b
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-02 17:00:06 +0800

    update test

--- index.php
+++ index.php
@@ -7,8 +7,14 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
-use App\Config;
+use Lib\URL;
 
 Dotenv::createImmutable(__DIR__)->load();
 
+echo "<pre>";
 
+$url = new URL("https://domain.com/asdasd?a=1&b=2#sdfsd");
+echo $url->get(), PHP_EOL;
+print_r($url->extract());
+echo $url->addQuery(['a' => 4, 'b' => 5])->get();
+echo "</pre>";
