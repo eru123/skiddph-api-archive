d800eb3956ce2b28712a1cfec9323caa6c6084bb
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-02 16:58:32 +0800

    update router

--- src/Lib/Router.php
+++ src/Lib/Router.php
@@ -4,4 +4,12 @@ namespace Lib;
 
 class Router
 {
+    static function isMatch(string $url, string $pattern): bool
+    {
+        $pattern = str_replace('/', '\/', $pattern);
+        $pattern = str_replace('*', '.*', $pattern);
+        $pattern = str_replace('?', '.?', $pattern);
+        $pattern = "/^$pattern$/";
+        return preg_match($pattern, $url);
+    }
 }
