43eee9632435b8193592bdf0143a9e2d7aff322f
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-11 00:15:46 +0800

    update

--- index.php
+++ index.php
@@ -6,10 +6,24 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo "<pre>";
+// echo "<pre>";
+echo "START", PHP_EOL;
 
 \Api\Core\Bootstrapper::init(__DIR__);
 $orm = \Api\Database\Database::connect('default');
 
-echo "OK";
-echo "</pre>";
+// print_r($orm->table('auth_users'));
+echo $orm->table('auth_users')
+    ->insert([
+        [
+            'user' => 'admin4',
+            'hash' => password_hash('pass', PASSWORD_BCRYPT, ['cost' => 12]),
+            'created_at' => date('Y-m-d H:i:s'),
+        ]
+    ])->rowCount(), PHP_EOL;
+
+// echo last inserted id
+echo $orm->lastInsertId(), PHP_EOL;
+
+echo PHP_EOL, "END";
+// echo "</pre>";
--- system/Database/Parser.php
+++ system/Database/Parser.php
@@ -5,9 +5,27 @@ namespace Api\Database;
 class Parser
 {
     final public static function parse(array $query): string
-    {
-        $sql = '';
-        
+    {   
+        // echo "QUERY: ", print_r($query, true), PHP_EOL;
+        $action = @$query['action'];
+        $action = "action_" . strtolower($action);
+        $sql = @self::{$action}($query);
+        // echo "SQL: ", $sql, PHP_EOL;
         return $sql;
+    }
+
+    final public static function action_insert(array $query): string
+    {
+        $table = @$query['table'];
+        $data = @$query['data'];
+        $columns = array_keys($data[0]);
+        $values = array_map(function ($row) {
+            return array_values($row);
+        }, $data);
+        $columns = implode(', ', $columns);
+        $values = implode(', ', array_map(function ($row) {
+            return '(' . implode(', ', $row) . ')';
+        }, $values));
+        return "INSERT INTO $table ($columns) VALUES $values";
     }
 }
