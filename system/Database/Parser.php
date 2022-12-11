89a2434a986e0e538995829870e3e27b76d1a02c
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-11 21:25:19 +0800

    added exception

--- system/Database/Parser.php
+++ system/Database/Parser.php
@@ -2,15 +2,19 @@
 
 namespace Api\Database;
 
+use Exception;
+
 class Parser
 {
     final public static function parse(array $query): string
-    {   
-        // echo "QUERY: ", print_r($query, true), PHP_EOL;
+    {
         $action = @$query['action'];
         $action = "action_" . strtolower($action);
-        $sql = @self::{$action}($query);
-        // echo "SQL: ", $sql, PHP_EOL;
+        try {
+            $sql = @self::{$action}($query);
+        } catch (Exception $e) {
+            throw new Exception("Invalid query action: $action");
+        }
         return $sql;
     }
 
