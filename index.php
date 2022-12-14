5ec03712c92e5c5b62fe64c74df358da8dee53d1
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-14 15:15:47 +0800

    update tests

--- index.php
+++ index.php
@@ -8,37 +8,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Api\Lib\Date;
 
-// echo "<pre>";
 echo "START", PHP_EOL;
-
-// Bootstrapper::init(__DIR__);
-// $orm = Database::connect('default');
-
-// // print_r($orm->table('auth_users'));
-// echo $orm->table('auth_users')
-//     ->insert([
-//         [
-//             'user' => 'admin1',
-//             'hash' => password_hash('pass', PASSWORD_BCRYPT, ['cost' => 12]),
-//             'created_at' => $orm->f("NOW()"),
-//         ]
-//     ])->rowCount(), PHP_EOL;
-
-// // echo last inserted id
-// echo $orm->lastInsertId(), PHP_EOL;
-
-echo Date::parse("1d", "h"), PHP_EOL;
-echo Date::parse("1d + 1h", "h"), PHP_EOL;
-echo Date::parse("1h - 1min", "m"), PHP_EOL;
-echo Date::parse("1minute + 0mins", "s"), PHP_EOL;
-echo Date::parse("1minute + 0mins", "ms"), PHP_EOL;
-echo Date::parse("now", "datetime"), PHP_EOL;
-echo Date::parse("time", "date"), PHP_EOL;
-echo Date::parse("now", "time"), PHP_EOL;
-echo Date::parse("now", "timestamp"), PHP_EOL;
-// echo Date::parse("now", "h:i:s A", Date::FORMAT), PHP_EOL;
-// echo Date::parse("now + 1h", "h:i:s A", Date::FORMAT), PHP_EOL;
-// echo Date::parse("2000-01-01 00:00:00 + 1d - 1s", "Y-m-d H:i:s", Date::FORMAT), PHP_EOL;
-
+echo Date::parse("1day ago", "date"), PHP_EOL;
+echo Date::parse("now before 1 day", "date"), PHP_EOL;
+echo Date::parse("now", "date"), PHP_EOL;
+echo Date::parse("now after 1 day", "date"), PHP_EOL;
 echo PHP_EOL, "END", PHP_EOL;
-// echo "</pre>";
