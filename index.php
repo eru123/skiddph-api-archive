c203f7cf65b49203f597fab22d2df49541b763cc
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-14 17:59:57 +0800

    remove firebase/php-jwt

--- index.php
+++ index.php
@@ -6,15 +6,23 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+Bootstrapper::init(__DIR__);
+
 use Api\Lib\Date;
+use Api\Auth\JWT;
 
 echo "START", PHP_EOL;
-echo Date::parse("1day ago", "date"), PHP_EOL;
-echo Date::parse("now before 1 day", "date"), PHP_EOL;
-echo Date::parse("1day ago + 24 hr", "date", Date::UNIT), PHP_EOL;
-echo Date::parse("now", "datetime", Date::UNIT), PHP_EOL;
-echo Date::parse("now", "Y-m-d h:i A", Date::FORMAT), PHP_EOL;
-echo Date::parse("now after 1 day", "date"), PHP_EOL;
-echo Date::parse("now + 1 day - (1min * 60 * 24) + (24 * (10 minutes * 6))", "date"), PHP_EOL;
-echo Date::parse("now + 1 day - (1min * 60 * 24) + (24 * (10 minutes * 6))", "date"), PHP_EOL;
+$cfg = Auth::config();
+// echo print_r($cfg->all(), true), PHP_EOL;
+$token = JWT::encode(['user_id' => 1]);
+echo $token, PHP_EOL;
+echo print_r(JWT::decode($token), true), PHP_EOL;
+// echo Date::parse("1day ago", "date"), PHP_EOL;
+// echo Date::parse("now before 1 day", "date"), PHP_EOL;
+// echo Date::parse("1day ago + 24 hr", "date", Date::UNIT), PHP_EOL;
+// echo Date::parse("now", "datetime", Date::UNIT), PHP_EOL;
+// echo Date::parse("now", "Y-m-d h:i A", Date::FORMAT), PHP_EOL;
+// echo Date::parse("now after 1 day", "date"), PHP_EOL;
+// echo Date::parse("now + 1 day - (1min * 60 * 24) + (24 * (10 minutes * 6))", "date"), PHP_EOL;
+// echo Date::parse("now + 1 day - (1min * 60 * 24) + (24 * (10 minutes * 6))", "date"), PHP_EOL;
 echo PHP_EOL, "END", PHP_EOL;
