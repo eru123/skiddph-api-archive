876a212dbdce5338e0b5ff17830c87f1c87f95c9
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-14 15:15:41 +0800

    remove pass to auth main class

--- system/Core/Auth.php
+++ system/Core/Auth.php
@@ -35,36 +35,4 @@ class Auth extends Plugin
         $cfg = self::config();
         return Database::connect($cfg->get('DB_ENV'));
     }
-
-    /**
-     * Get Password hash method
-     * @return array
-     */
-    final static function getHashMethod(): array
-    {
-        $cfg = self::config();
-        $default_opts = [PASSWORD_BCRYPT, ['cost' => 12]];
-        return $cfg->has('HASH_METHOD') ? $cfg->get('HASH_METHOD') : $default_opts;
-    }
-
-    /**
-     * Hash a password.
-     * @param   string  $password   The password to hash.
-     * @return  string
-     */
-    final static function hash(string $password): string
-    {
-        return password_hash($password, ...self::getHashMethod());
-    }
-
-    /**
-     * Verify a password hash.
-     * @param   string  $password   The password to verify.
-     * @param   string  $hash       The hash to verify against.
-     * @return  bool
-     */
-    final static function verify(string $password, string $hash): bool
-    {
-        return password_verify($password, $hash);
-    }
 }
