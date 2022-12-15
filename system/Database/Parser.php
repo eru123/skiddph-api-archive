28ef0dba5b948d6eea0759373c7e4bdff9df6a53
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-15 18:26:50 +0800

    added action update

--- system/Database/Parser.php
+++ system/Database/Parser.php
@@ -34,4 +34,50 @@ class Parser
         }, $values));
         return "INSERT INTO $table ($columns) VALUES $values";
     }
+
+    final public static function action_update(array $query): string
+    {
+        $table = @$query['table'];
+        $data = @$query['data'];
+        $where = @$query['where'];
+        $columns = array_keys($data[0]);
+        $values = array_map(function ($row) {
+            return array_values($row);
+        }, $data);
+        $columns = implode(', ', $columns);
+        $values = implode(', ', array_map(function ($row) {
+            return '(' . implode(', ', $row) . ')';
+        }, $values));
+
+        if (is_array($where)) {
+            $where = self::parse_where($where);
+        }
+
+        return "UPDATE $table SET ($columns) = $values WHERE $where";
+    }
+
+    final public static function parse_where(array $where): string
+    {
+        $sql = [];
+        foreach ($where as $key => $value) {
+            // if is numeric, then is a key
+            if (is_numeric($key)) {
+                if (is_array($value)) {
+                    $sql[] = "(" . self::parse_where($value) . ")";
+                } else {
+                    $sql[] = $value;
+                }
+            } else if (is_string($key)) {
+                if (is_array($value)) {
+                    $sql[] = Helper::isMultiArray($value) ? "(" . Helper::condition($key, $value) . ")" : Helper::condition($key, $value);
+                } else {
+                    $sql[] = "$key = $value";
+                }
+            }
+        }
+
+
+
+        return implode(' ', $sql);
+    }
 }
