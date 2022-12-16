d8c9607749d7c21e2b9e513dc372082a92526d4e
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-16 12:07:09 +0800

    update parser

--- system/Database/Parser.php
+++ system/Database/Parser.php
@@ -9,6 +9,7 @@ class Parser
     final public static function parse(array $query): string
     {
         echo "QUERY: ", print_r($query, true), PHP_EOL;
+        $query = self::filterData($query);
         $action = @$query['action'];
         $action = "action_" . strtolower($action);
         try {
@@ -40,27 +41,23 @@ class Parser
         $table = @$query['table'];
         $data = @$query['data'];
         $where = @$query['where'];
-        $columns = array_keys($data[0]);
-        $values = array_map(function ($row) {
-            return array_values($row);
-        }, $data);
-        $columns = implode(', ', $columns);
-        $values = implode(', ', array_map(function ($row) {
-            return '(' . implode(', ', $row) . ')';
-        }, $values));
 
-        if (is_array($where)) {
-            $where = self::parse_where($where);
+        $set = [];
+        foreach ($data as $value) {
+            foreach ($value as $key => $val) {
+                $set[] = "$key = $val";
+            }
         }
 
-        return "UPDATE $table SET ($columns) = $values WHERE $where";
+        $set = implode(', ', $set);
+        $where = is_array($where) ? self::parse_where($where) : $where;
+        return "UPDATE $table SET $set WHERE $where";
     }
 
     final public static function parse_where(array $where): string
     {
         $sql = [];
         foreach ($where as $key => $value) {
-            // if is numeric, then is a key
             if (is_numeric($key)) {
                 if (is_array($value)) {
                     $sql[] = "(" . self::parse_where($value) . ")";
@@ -76,8 +73,34 @@ class Parser
             }
         }
 
-
-
         return implode(' ', $sql);
+    }
+
+    final public static function action_delete(array $query): string
+    {
+        $table = @$query['table'];
+        $where = @$query['where'];
+        $where = is_array($where) ? self::parse_where($where) : $where;
+        return "DELETE FROM $table WHERE $where";
+    }
+
+    final public static function action_select(array $query): string
+    {
+        $table = @$query['table'];
+        $columns = @$query['columns'];
+        $where = @$query['where'];
+        $where = is_array($where) ? self::parse_where($where) : $where;
+        $columns = is_array($columns) ? implode(', ', $columns) : $columns;
+        return "SELECT $columns FROM $table WHERE $where";
+    }
+
+    final public static function filterData(array $query): array
+    {   
+        if(isset($query['data'])) {
+            $data = $query['data'];
+            $query['data'] = Helper::isMultiArray($data) ? $data : [$data];
+        }
+        
+        return $query;
     }
 }
