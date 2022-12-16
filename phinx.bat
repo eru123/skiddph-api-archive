6a4fcc1f407e32033cfd19e5063f205cc1782dcc
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-16 18:24:25 +0800

    added extra reset command on win

--- phinx.bat
+++ phinx.bat
@@ -3,5 +3,12 @@
 REM Get arguments
 @set args=%*
 
+REM If first arg is "reset"
+if "%1"=="reset" (
+    php "%~dp0vendor\bin\phinx" rollback -t 0
+    php "%~dp0vendor\bin\phinx" migrate
+    exit /b
+)
+
 REM Run Phinx
 php "%~dp0vendor\bin\phinx" %args%
