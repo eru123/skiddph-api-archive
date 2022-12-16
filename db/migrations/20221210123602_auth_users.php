13a8ada1a42bf8622b8636ab8b178ea1e4cb7bee
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-16 18:23:13 +0800

    update auth users

--- db/migrations/20221210123602_auth_users.php
+++ db/migrations/20221210123602_auth_users.php
@@ -6,26 +6,16 @@ use Phinx\Migration\AbstractMigration;
 
 final class AuthUsers extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
     {
         $table = $this->table('auth_users');
         $table->addColumn('user', 'string', ['limit' => 255, 'null' => false])
             ->addColumn('hash', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('last_user', 'string', ['limit' => 255, 'null' => true])
+            ->addColumn('last_hash', 'string', ['limit' => 255, 'null' => true])
             ->addColumn('created_at', 'datetime', ['null' => true])
             ->addColumn('updated_at', 'datetime', ['null' => true])
-            ->addColumn('deleted_at', 'datetime', ['null' => true])
-            ->addColumn('deactivated_at', 'datetime', ['null' => true])
+            ->addColumn('status', 'string', ['limit' => 255, 'default' => 'active', 'null' => false])
             ->addIndex(['user'], ['unique' => true])
             ->create();
     }
