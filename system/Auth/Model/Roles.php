5c9feb605272e9df93876e5c84564519ff02bb9f
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-20 17:49:30 +0800

    update role model

--- system/Auth/Model/Roles.php
+++ system/Auth/Model/Roles.php
@@ -12,4 +12,69 @@ class Roles extends Model
     {
         parent::__construct(Auth::db(), self::TB);
     }
+
+    public static function set(int $user_id, $role)
+    {
+        $roles = [];
+        if (is_array($role)) {
+            $roles = $role;
+        } else {
+            $delims = ['|', ',', ' '];
+            foreach ($delims as $delim) {
+                if (strpos($role, $delim) !== false) {
+                    $roles = explode($delim, $role);
+                    break;
+                }
+            }
+        }
+        $roles = array_map(function ($role) use ($user_id) {
+            $role = trim($role);
+            $role = strtoupper($role);
+            return [
+                'user_id' => $user_id,
+                'role' => $role
+            ];
+        }, $roles);
+
+        $orm = Auth::db();
+
+        $orm->table(self::TB)
+            ->where([
+                'user_id' => $user_id
+            ])
+            ->delete();
+
+        return $orm->table(self::TB)
+            ->data($roles)->rowCount() > 0;
+    }
+
+    public static function get(int $user_id)
+    {
+        $roles = Auth::db()->table(self::TB)
+            ->select('role')
+            ->where([
+                'user_id' => $user_id
+            ])
+            ->readMany()
+            ->arr();
+
+        return array_map(function ($role) {
+            return $role['role'];
+        }, $roles);
+    }
+
+    public static function has(int $user_id, string $role)
+    {
+        $role = strtoupper($role);
+        $roles = Auth::db()->table(self::TB)
+            ->select('role')
+            ->where([
+                'user_id' => $user_id,
+                'role' => $role
+            ])
+            ->readOne()
+            ->arr();
+
+        return !empty($roles);
+    }
 }
