c5d5508f42a8bdfe175f7127d4180c01e2441d33
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-20 17:49:48 +0800

    update user model

--- system/Auth/Model/Users.php
+++ system/Auth/Model/Users.php
@@ -12,4 +12,28 @@ class Users extends Model
     {
         parent::__construct(Auth::db(), self::TB);
     }
+
+    public static function user(int $user_id)
+    {
+        $user = Auth::db()->table(self::TB)
+            ->where([
+                'id' => $user_id
+            ])
+            ->readOne()
+            ->arr();
+        return empty($user) ? null : $user;
+    }
+
+    public static function users(array $user_ids)
+    {
+        $users = Auth::db()->table(self::TB)
+            ->where([
+                'id' => [
+                    'IN' => $user_ids
+                ]
+            ])
+            ->readMany()
+            ->arr();
+        return empty($users) ? [] : $users;
+    }
 }
