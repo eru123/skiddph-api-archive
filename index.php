1a6f69329bc68f4fc33cc46b26383d371abe29fc
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-22 14:31:56 +0800

    update

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-use Api\Auth\Model\Info;
-use Api\Auth\User;
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -13,10 +10,25 @@ Bootstrapper::init(__DIR__);
 
 echo "START", PHP_EOL;
 
-$data = Auth::login('admin', 'pass');
-$refresh = Auth::refreshToken($data['token'], $data['refresh_token']);
+// $data = Auth::register('admin4', 'pass', 'superadmin,admin, moderator', [
+//     'email' => [
+//         'admin4@localhost',
+//         'admin4@xyz'
+//     ],
+//     'name' => 'Admin4',
+//     'surname' => 'Admin4',
+//     'phone' => '2123456789',
+//     'profile' => [
+//         'name' => 'Admin4',
+//         'surname' => 'Admin4',
+//         'phone' => '2123456789',
+//     ]
+// ]);
+
+$data = Auth::login('admin4', 'pass');
+// $refresh = Auth::refreshToken($data['token'], $data['refresh_token']);
 
 echo json_encode($data, JSON_PRETTY_PRINT), PHP_EOL;
-echo json_encode($refresh, JSON_PRETTY_PRINT), PHP_EOL;
+// echo json_encode($refresh, JSON_PRETTY_PRINT), PHP_EOL;
 
 echo PHP_EOL, "END", PHP_EOL;
