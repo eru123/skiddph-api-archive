54b25a15b581c2b733d1865b5624ba3eea9b92ef
ipp-jericho <jerico.aquino@intl-payments.com>
2022-12-22 18:01:56 +0800

    update entry

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-Bootstrapper::init(__DIR__);
-
-echo "START", PHP_EOL;
-
-// $data = Auth::register('admin4', 'pass', 'superadmin,admin, moderator', [
-//     'email' => [
-//         'admin4@localhost',
-//         'admin4@xyz'
-//     ],
-//     'name' => 'Admin4',
-//     'surname' => 'Admin4',
-//     'phone' => '2123456789',
-//     'profile' => [
-//         'name' => 'Admin4',
-//         'surname' => 'Admin4',
-//         'phone' => '2123456789',
-//     ]
-// ]);
-
-$data = Auth::login('admin4', 'pass');
-// $refresh = Auth::refreshToken($data['token'], $data['refresh_token']);
-
-echo json_encode($data, JSON_PRETTY_PRINT), PHP_EOL;
-// echo json_encode($refresh, JSON_PRETTY_PRINT), PHP_EOL;
-
-echo PHP_EOL, "END", PHP_EOL;
+require_once __DIR__ . '/api/index.php';
