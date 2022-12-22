773a2754d2702268fa3aa958d4dea733c5e2c807
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-22 18:08:23 +0800

    fix deprecated

--- system/Core/Bootstrapper.php
+++ system/Core/Bootstrapper.php
@@ -10,14 +10,17 @@ class Bootstrapper
 
     public static function load(string $cwd = null): void
     {
-        if ($cwd === null) $cwd = getcwd();
-        if (is_dir($cwd)) $cwd = realpath($cwd);
-        else return;
+        if ($cwd === null)
+            $cwd = getcwd();
+        if (is_dir($cwd))
+            $cwd = realpath($cwd);
+        else
+            return;
 
         Dotenv::createImmutable($cwd)->load();
         $files = scandir($cwd, SCANDIR_SORT_ASCENDING);
         foreach ($files as $file) {
-            if (is_file($file)) {
+            if (file_exists($file)) {
                 $file_info = pathinfo($file);
                 if (@$file_info["extension"] && in_array($file_info["extension"], self::config_ext) && $file_info["filename"] === self::config_name) {
                     self::$config_path = $cwd . DIRECTORY_SEPARATOR . $file;
@@ -29,10 +32,13 @@ class Bootstrapper
 
     public static function init(string $dir = null): void
     {
-        if ($dir) self::load($dir);
+        if ($dir) {
+            self::load($dir);
+        }
+
         $cfg_file = self::$config_path;
 
-        if (is_file($cfg_file)) {
+        if (!empty($cfg_file) && file_exists($cfg_file)) {
             $config = require $cfg_file;
             foreach ($config as $key => $value) {
                 $plugin = new PluginConfig($key);
