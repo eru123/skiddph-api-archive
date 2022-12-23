947ba6f344bf4cd1c3af0a2d0736ec55592fef63
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-23 15:01:05 +0800

    update auth

--- api/v1/auth.php
+++ api/v1/auth.php
@@ -4,10 +4,24 @@ $router = new Router();
 $router->base('/auth');
 
 $router->get('/user/{id}/edit', function ($p) {
+    Auth::accessControl('superadmin,admin');
     return [
         'id' => $p['id'],
         'name' => 'John Doe',
     ];
+});
+
+$router->post('/signin', function () {
+    $body = Request::bodySchema([
+        'user' => [
+            'required' => true,
+        ],
+        'pass' => [
+            'required' => true,
+        ],
+    ]);
+
+    return Auth::login($body['user'], $body['pass']);
 });
 
 return $router;
