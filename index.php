4c6a8d454a78c9301c42d30f1d3033f0ac3e7a07
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-23 15:00:37 +0800

    update routing

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-require_once __DIR__ . '/api/index.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/vendor/autoload.php';
+Bootstrapper::init(__DIR__);
+
+$api = require __DIR__ . '/api/index.php';
+
+$router = new Router();
+$router->base('/');
+
+$router->add($api);
+$router->run();
