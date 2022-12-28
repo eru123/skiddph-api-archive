5de0160bccb269ed99f0fcf0c09a4650e307f3fb
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-28 08:04:48 +0800

    move auth

--- api/v1/url.php
+++ api/v1/url.php
@@ -5,8 +5,4 @@ use Plugin\URL\Controller;
 $router = new Router();
 $router->base('/url');
 
-$router->post('/signin', [Controller::class, 'signin']);
-$router->post('/signup', [Controller::class, 'signup']);
-$router->post('/verify/resend/email', [Controller::class, 'resendEmail']);
-$router->post('/verify/email/{verifyId}', [Controller::class, 'verifyEmail']);
 return $router;
