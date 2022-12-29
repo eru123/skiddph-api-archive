fef67432f8e3d9ce510156553dbd9149ca68879d
eru123 <39824083+eru123@users.noreply.github.com>
2022-12-29 15:44:21 +0800

    start session

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 Bootstrapper::init(__DIR__);
 
