0f0b7c7d29a9ce91811f9de8bcb5ed14549b6cf6
eru123 <39824083+eru123@users.noreply.github.com>
2023-01-11 12:56:07 +0800

    update

--- system/Plugin/Auth/Password.php
+++ system/Plugin/Auth/Password.php
@@ -5,24 +5,13 @@ namespace SkiddPH\Plugin\Auth;
 class Password
 {
     /**
-     * Get Password hash method
-     * @return array
-     */
-    final static function getHashMethod(): array
-    {
-        $cfg = Auth::config();
-        $default_opts = [PASSWORD_BCRYPT, ['cost' => 12]];
-        return $cfg->has('HASH_METHOD') ? $cfg->get('HASH_METHOD') : $default_opts;
-    }
-
-    /**
      * Hash a password.
      * @param   string  $password   The password to hash.
      * @return  string
      */
     final static function hash(string $password): string
     {
-        return password_hash($password, ...self::getHashMethod());
+        return password_hash($password, ...pcfg('auth.hash_method', [PASSWORD_BCRYPT, ['cost' => 12]]));
     }
 
     /**
