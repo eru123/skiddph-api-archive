0e0c42b706fa2e05565cd7c3a1d15b4c2b1c290b
eru123 <39824083+eru123@users.noreply.github.com>
2023-01-19 17:29:38 +0800

    update bootstrapper

--- system/Core/Bootstrapper.php
+++ system/Core/Bootstrapper.php
@@ -7,12 +7,17 @@ use SkiddPH\Helper\Date;
 
 class Bootstrapper
 {
+    static $initiated = false;
     const config_ext = ["php"];
     const config_name = "config";
     const config_dir = "config";
 
     public static function init(string $dir = null): void
     {
+        if (self::$initiated)
+            return;
+        self::$initiated = true;
+
         $data = [];
 
         if ($dir === null)
@@ -26,49 +31,31 @@ class Bootstrapper
         $files = scandir($dir, SCANDIR_SORT_ASCENDING);
         foreach ($files as $file) {
             if (file_exists($file)) {
-                $file_info = pathinfo($file);
+                $finfo = pathinfo($file);
                 if (
-                    @$file_info["extension"] &&
-                    in_array($file_info["extension"], self::config_ext) &&
-                    $file_info["filename"] === self::config_name &&
+                    @$finfo["extension"] &&
+                    in_array($finfo["extension"], self::config_ext) &&
+                    $finfo["filename"] === self::config_name &&
                     file_exists($dir . DIRECTORY_SEPARATOR . $file)
                 ) {
-                    $data = require($dir . DIRECTORY_SEPARATOR . $file);
-                    $data = is_array($data) ? $data : [];
-                    $data = array_merge($data, $data);
+                    $fdata = require($dir . DIRECTORY_SEPARATOR . $file);
+                    $fdata = is_array($data) ? $data : [];
+                    $data = array_merge($data, $fdata);
+                    sys('plugins', $data);
+                    $fdata = null;
+                    $data = null;
+                    $finfo = null;
+                    $file = null;
+                    $files = null;
                     break;
                 }
             }
         }
 
         $config_dir = $dir . DIRECTORY_SEPARATOR . self::config_dir;
-        if (is_dir($config_dir)) {
-            $files = scandir($config_dir, SCANDIR_SORT_ASCENDING);
-            foreach ($files as $file) {
-                $file = $config_dir . DIRECTORY_SEPARATOR . $file;
-                if (file_exists($file)) {
-                    $file_info = pathinfo($file);
-                    if (
-                        @$file_info["extension"] &&
-                        in_array($file_info["extension"], self::config_ext) &&
-                        file_exists($file)
-                    ) {
-                        $data[$file_info["filename"]] = require($file);
-                    }
-                }
-            }
-        }
-
-        sys('plugins', $data);
-        sys('timezone', pcfg('app.timezone', date_default_timezone_get()));
-
-        // Set Timezone
-        date_default_timezone_set(sys('timezone'));
-
-        // Set Date and Time
-        sys('time', time());
-        sys('date', date('Y-m-d H:i:s', sys('time')));
-        Date::setTime(sys('time'));
+        $pre = [];
+        $post = [];
+        $on = [];
 
         // System Specifics
         sys('env', @$_ENV ?? []);
@@ -79,5 +66,60 @@ class Bootstrapper
         sys('get', @$_GET ?? []);
         sys('post', @$_POST ?? []);
         sys('files', @$_FILES ?? []);
+        sys('workdir', $dir);
+        sys('configdir', $config_dir);
+
+        if (is_dir($config_dir)) {
+            $files = scandir($config_dir, SCANDIR_SORT_ASCENDING);
+            foreach ($files as $file) {
+                $file = $config_dir . DIRECTORY_SEPARATOR . $file;
+                if (file_exists($file)) {
+                    $finfo = pathinfo($file);
+                    if (
+                        @$finfo["extension"] &&
+                        in_array($finfo["extension"], self::config_ext) &&
+                        file_exists($file)
+                    ) {
+                        if (preg_match('/^(.*)\.pre\.php$/', $finfo["basename"], $matches)) {
+                            $pre[$matches[1]] = $file;
+                        } else if (preg_match('/^(.*)\.post\.php$/', $finfo["basename"], $matches)) {
+                            $post[$matches[1]] = $file;
+                        } else if (preg_match('/^(.*)\.php$/', $finfo["basename"], $matches)) {
+                            $on[$matches[1]] = $file;
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach ($pre as $key => $file) {
+            $fdata = require($file);
+            sys("plugins.$key", $fdata);
+            if ($key === 'app') {
+                sys('timezone', pcfg('app.timezone'));
+                datetime_init();
+            }
+        }
+
+        if (!pcfg('app.timezone')) {
+            datetime_init();
+        }
+
+        foreach ($on as $key => $file) {
+            $fdata = require($file);
+            sys("plugins.$key", $fdata);
+        }
+
+        foreach ($post as $key => $file) {
+            $fdata = require($file);
+            sys("plugins.$key", $fdata);
+        }
+
+        $on = null;
+        $pre = null;
+        $post = null;
+        $files = null;
+        $key = null;
+        $file = null;
     }
 }
