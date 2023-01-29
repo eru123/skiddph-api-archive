921d13728c955928433b3a6d92b5222efc2b568c
eru123 <39824083+eru123@users.noreply.github.com>
2023-01-30 00:52:33 +0800

    smtp test

--- test.php
+++ test.php
@@ -3,57 +3,14 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use SkiddPH\Core\Bootstrapper;
-use SkiddPH\Model\User;
-use SkiddPH\Plugin\DB\DB;
+use SkiddPH\Helper\Date;
+use SkiddPH\Plugin\SMTP\SMTP;
 
 Bootstrapper::init(__DIR__);
 
-// $user = User::first('user', 'admin');
-// if ($user) {
-//     echo json_encode($user->strip()->array(), JSON_PRETTY_PRINT) . PHP_EOL;
-//     $user->delete();
-// }
-
-// $user = User::first('user', 'admin123');
-// if ($user) {
-//     echo json_encode($user->strip()->array(), JSON_PRETTY_PRINT) . PHP_EOL;
-//     $user->delete();
-// }
-
-
-
-// for ($i = 0; $i < 1000; $i++) {
-//     try {
-//         $user = User::create();
-
-//         $user->user = 'admin_' . $i;
-//         $user->hash = 'pass';
-//         $user->save();
-//     } catch (Exception $e) {
-//         echo $e->getMessage() . PHP_EOL;
-//     }
-// }
-
-$users = User::get();
-// $effected = $users->update([
-//     'status' => 'active'
-// ]);
-
-// echo 'Effected: ' . $effected . PHP_EOL;
-
-// // foreach ($users as $user) {
-// //     echo 'X >> ' . json_encode($user->strip()->array(), JSON_PRETTY_PRINT) . PHP_EOL;
-// // }
-
-
-var_dump($users->count());
-var_dump($users->delete());
-
-// $alt = User::first('user', 'admin123');
-// if ($alt) {
-//     $alt->delete();
-// }
-// sleep(1);
-// $user->user = 'admin123';
-// $user->updated_at = DB::raw('NOW()');
-// $user->update();
+$smtp = SMTP::use();
+$smtp->to('yeoligoakino@gmail.com');
+$smtp->subject('DateTime: '.Date::parse('now', 'datetime'));
+$smtp->text('Test');
+$smtp->html();
+$smtp->send();
