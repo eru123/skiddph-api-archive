af2fe22ce19f9cb566cd5965c9a0e0e0e33d7550
eru123 <39824083+eru123@users.noreply.github.com>
2023-04-01 15:32:08 +0000

    make orm builtin

---
+++ system/eru123/orm/ORM.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eru123\orm;
+use eru123\orm\ORM\Management;
+use eru123\orm\ORM\Model;
+
+class ORM
+{
+    use Management;
+    use Model;
+}
---
+++ system/eru123/orm/Table/CRUD.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace eru123\orm\Table;
+
+use eru123\orm\Table\CRUD\Delete;
+use eru123\orm\Table\CRUD\Insert;
+use eru123\orm\Table\CRUD\Select;
+use eru123\orm\Table\CRUD\Update;
+
+trait CRUD
+{
+    use Insert;
+    use Update;
+    use Delete;
+    use Select;
+}
