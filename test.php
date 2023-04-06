0d8723e6df586babd989e50900bc45059796203f
eru123 <39824083+eru123@users.noreply.github.com>
2023-04-06 18:19:43 +0800

    up

--- test.php
+++ test.php
@@ -9,13 +9,29 @@ Bootstrapper::init(__DIR__);
 
 $orm = ORM::createFromComposer([ 'SkiddPH', 'eru123' ], __DIR__);
 
-// $orm->EmailAttachments()->insert([
-//     'email_id' => 1,
-//     'path' => 'test.txt'
-// ]);
+$orm->EmailAttachments()->insert([
+    'email_id' => 1,
+    'path' => 'test.txt'
+]);
 
 $orm->EmailLogs()->insert([
     'email_id' => 1,
     'message' => 'sent',
     'created_at' => date('Y-m-d H:i:s')
+]);
+
+$orm->EmailQueues()->insert([
+    'to' => 'hello@jericho.work',   
+    'cc' => 'jericho@skiddph.com',
+    'bcc' => null,
+    'subject' => 'Test',
+    'body' => 'Test',
+    'priority' => 1,
+    'for_approval' => false,
+    'approved_at' => null,
+    'pending_at' => null,
+    'delivered_at' => null,
+    'failed_at' => null,
+    'created_at' => date('Y-m-d H:i:s'),
+    'deleted_at' => null
 ]);
